712b54e5da4ea3d6fc691abdb0292a1249e38c2f
Pedro Bellezia <pedrodalcontato@gmail.com>
2026-05-05 13:16:46 -0300

    criar arquivo de inicialização da API

---
+++ app/app.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from app.router import agency_router, client_router, user_router, writer_router
+
+app = FastAPI(
+    title="Fenix API",
+    redirect_slashes=False
+)
+
+app.include_router(agency_router, prefix="/agency", tags=["agency"])
+app.include_router(client_router, prefix="/client", tags=["client"])
+app.include_router(user_router, prefix="/user", tags=["user"])
+app.include_router(writer_router, prefix="/writer", tags=["writer"])
