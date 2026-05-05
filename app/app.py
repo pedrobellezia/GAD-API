326021c1a2aada0aadab2e25886d12203cc8a46f
Pedro Bellezia <pedrodalcontato@gmail.com>
2026-05-05 16:26:37 -0300

    remover o parametro echo da engine do sqlalchemyclear

--- app/app.py
+++ app/app.py
@@ -1,3 +1,5 @@
+from os import getenv
+
 from fastapi import FastAPI
 
 from app.router import agency_router, client_router, user_router, writer_router
@@ -8,3 +10,13 @@ app.include_router(agency_router, prefix="/agency", tags=["agency"])
 app.include_router(client_router, prefix="/client", tags=["client"])
 app.include_router(user_router, prefix="/user", tags=["user"])
 app.include_router(writer_router, prefix="/writer", tags=["writer"])
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        "app.app:app",
+        host=getenv("HOST"),
+        port=int(getenv("PORT")),
+        reload=True,
+    )
