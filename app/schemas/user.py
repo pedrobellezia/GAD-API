9b46c955c704210f901e59064450de9ebd256ff2
Pedro Bellezia <pedrodalcontato@gmail.com>
2026-05-06 18:35:49 -0300

    adicionar validação simples de email

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -2,14 +2,14 @@ from datetime import datetime
 from typing import Optional
 from uuid import UUID
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, EmailStr
 
 from app.models import UserType
 
 
 class UserCreate(BaseModel):
     name: str
-    email: str
+    email: EmailStr
     type: UserType
     avatar: Optional[str] = None
     pswd: str
@@ -17,7 +17,7 @@ class UserCreate(BaseModel):
 
 class UserRead(BaseModel):
     id: UUID
-    name: str
+    name: EmailStr
     email: str
     type: UserType
     avatar: Optional[str] = None
@@ -29,7 +29,7 @@ class UserRead(BaseModel):
 class UserFilter(BaseModel):
     id: Optional[UUID] = None
     name: Optional[str] = None
-    email: Optional[str] = None
+    email: Optional[EmailStr] = None
     type: Optional[UserType] = None
     skip: Optional[int] = 0
     limit: Optional[int] = 100
