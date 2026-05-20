1e24b555ae262222959ff547149807bacc7409c0
Pedro Bellezia <pedrodalcontato@gmail.com>
2026-05-20 17:38:12 -0300

    refactor: adicionar um espaço para possíveis erros em retornos de status code 207

--- app/schemas/common.py
+++ app/schemas/common.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from pydantic import BaseModel
 
 from app.utils.types import NonEmptyStr
@@ -5,5 +7,6 @@ from app.utils.types import NonEmptyStr
 
 class DetailsResponse(BaseModel):
     details: NonEmptyStr
+    errors: list[dict[str, Any]]
 
     model_config = {"extra": "forbid"}
