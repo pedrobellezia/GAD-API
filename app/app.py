e038c30cf207fb55aab3e2643d2ce3b6f926b26c
Pedro Bellezia <pedrodalcontato@gmail.com>
2026-07-07 16:07:51 -0300

    feat(api): adicionar configuração inicial de CORS

--- app/app.py
+++ app/app.py
@@ -10,7 +10,7 @@ from app.exceptions import (
     integrity_error_handler,
 )
 from app.router import agency_router, auth_router, me_router, media_router
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="Fenix API")
 
 app.include_router(agency_router, prefix="/agency", tags=["agency"])
@@ -21,6 +21,14 @@ app.include_router(media_router, prefix="/media", tags=["media"])
 app.add_exception_handler(RequestValidationError, request_validation_handler)
 app.add_exception_handler(ResponseValidationError, response_validation_handler)
 app.add_exception_handler(IntegrityError, integrity_error_handler)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     import uvicorn
