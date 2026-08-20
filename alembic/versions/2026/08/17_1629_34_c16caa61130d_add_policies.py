f0a2fb339582aa8a0b33a2fb38a4d88906d6e326
Pedro Bellezia <pedrodalcontato@gmail.com>
2026-08-20 13:27:21 -0300

    fix(alembic): corrigir conteudo da migration de politicas sql

--- alembic/versions/2026/08/17_1629_34_c16caa61130d_add_policies.py
+++ alembic/versions/2026/08/17_1629_34_c16caa61130d_add_policies.py
@@ -17,20 +17,90 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
+    op.execute("ALTER TABLE agencies ENABLE ROW LEVEL SECURITY;")
+    op.execute("ALTER TABLE agencies FORCE ROW LEVEL SECURITY;")
+    op.execute("ALTER TABLE designers ENABLE ROW LEVEL SECURITY;")
+    op.execute("ALTER TABLE designers FORCE ROW LEVEL SECURITY;")
+    op.execute("ALTER TABLE writers ENABLE ROW LEVEL SECURITY;")
+    op.execute("ALTER TABLE writers FORCE ROW LEVEL SECURITY;")
+    op.execute("ALTER TABLE clients ENABLE ROW LEVEL SECURITY;")
+    op.execute("ALTER TABLE clients FORCE ROW LEVEL SECURITY;")
+    op.execute("ALTER TABLE users ENABLE ROW LEVEL SECURITY;")
+    op.execute("ALTER TABLE users FORCE ROW LEVEL SECURITY;")
+
     op.execute("""
-        SELECT cron.schedule(
-            'delete_soft_deleted_users',
-            '0 0 * * *',
-            $cron$
-            DELETE FROM users
-            WHERE deleted_at IS NOT NULL
-              AND deleted_at < NOW() - INTERVAL '1 month';
-            $cron$
-        );
+        CREATE POLICY hide_soft_deleted_users
+            ON users
+            AS RESTRICTIVE
+            FOR ALL
+            USING (
+            users.deleted_at is NULL
+            );
+    """)
+    op.execute("""
+        CREATE POLICY hide_soft_deleted_clients
+            ON clients
+            AS RESTRICTIVE
+            FOR ALL
+            USING (
+            EXISTS (SELECT 1
+                    FROM users
+                    WHERE users.id = clients.id
+                      AND users.deleted_at IS NULL)
+            );
+    """)
+    op.execute("""
+        CREATE POLICY hide_soft_deleted_writers
+            ON writers
+            AS RESTRICTIVE
+            FOR ALL
+            USING (
+            EXISTS (SELECT 1
+                    FROM users
+                    WHERE users.id = writers.id
+                      AND users.deleted_at IS NULL)
+            );
+    """)
+    op.execute("""
+        CREATE POLICY hide_soft_deleted_agencies
+            ON agencies
+            AS RESTRICTIVE
+            FOR ALL
+            USING (
+            EXISTS (SELECT 1
+                    FROM users
+                    WHERE users.id = agencies.id
+                      AND users.deleted_at IS NULL)
+            );
+    """)
+    op.execute("""
+        CREATE POLICY hide_soft_deleted_designers
+            ON designers
+            AS RESTRICTIVE
+            FOR ALL
+            USING (
+            EXISTS (SELECT 1
+                    FROM users
+                    WHERE users.id = designers.id
+                      AND users.deleted_at IS NULL)
+            );
     """)
 
 
 def downgrade() -> None:
-    op.execute("""
-        SELECT cron.unschedule('delete_soft_deleted_users');
-    """)
+    op.execute("DROP POLICY IF EXISTS hide_soft_deleted_designers ON designers;")
+    op.execute("DROP POLICY IF EXISTS hide_soft_deleted_agencies ON agencies;")
+    op.execute("DROP POLICY IF EXISTS hide_soft_deleted_writers ON writers;")
+    op.execute("DROP POLICY IF EXISTS hide_soft_deleted_clients ON clients;")
+    op.execute("DROP POLICY IF EXISTS hide_soft_deleted_users ON users;")
+
+    op.execute("ALTER TABLE users NO FORCE ROW LEVEL SECURITY;")
+    op.execute("ALTER TABLE users DISABLE ROW LEVEL SECURITY;")
+    op.execute("ALTER TABLE clients NO FORCE ROW LEVEL SECURITY;")
+    op.execute("ALTER TABLE clients DISABLE ROW LEVEL SECURITY;")
+    op.execute("ALTER TABLE writers NO FORCE ROW LEVEL SECURITY;")
+    op.execute("ALTER TABLE writers DISABLE ROW LEVEL SECURITY;")
+    op.execute("ALTER TABLE designers NO FORCE ROW LEVEL SECURITY;")
+    op.execute("ALTER TABLE designers DISABLE ROW LEVEL SECURITY;")
+    op.execute("ALTER TABLE agencies NO FORCE ROW LEVEL SECURITY;")
+    op.execute("ALTER TABLE agencies DISABLE ROW LEVEL SECURITY;")
