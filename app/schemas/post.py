7a08c0186dfde9a352ef3d96f3f3928b92bca281
Pedro Bellezia <152564698+pedrobellezia@users.noreply.github.com>
2026-08-25 18:38:49 -0300

    feat(post): permitir filtrar posts por múltiplos id, status, agency, client, writer (#25)

--- app/schemas/post.py
+++ app/schemas/post.py
@@ -42,34 +42,34 @@ class PostRead(BaseModel):
 
 
 class PostFilter(BaseModel):
-    id: UUID | None = None
+    id: list[UUID] | None = None
     title: str | None = None
-    status: PostStatus | None = None
-    agency_id: UUID | None = None
-    client_id: UUID | None = None
-    writer_id: UUID | None = None
+    status: list[PostStatus] | None = None
+    agency_id: list[UUID] | None = None
+    client_id: list[UUID] | None = None
+    writer_id: list[UUID] | None = None
     order_by: Literal["new", "old"] = "new"
     skip: int = Field(default=0, ge=0)
     limit: int = Field(default=100, ge=1, le=1000)
 
     def apply_filters(self, query):
         if self.id:
-            query = query.where(Post.id == self.id)
+            query = query.where(Post.id.in_(self.id))
 
         if self.title:
             query = query.where(Post.title.ilike(f"%{self.title}%"))
 
         if self.status:
-            query = query.where(Post.status == self.status)
+            query = query.where(Post.status.in_(self.status))
 
         if self.agency_id:
-            query = query.where(Post.agency_id == self.agency_id)
+            query = query.where(Post.agency_id.in_(self.agency_id))
 
         if self.client_id:
-            query = query.where(Post.client_id == self.client_id)
+            query = query.where(Post.client_id.in_(self.client_id))
 
         if self.writer_id:
-            query = query.where(Post.writer_id == self.writer_id)
+            query = query.where(Post.writer_id.in_(self.writer_id))
 
         if self.order_by == "old":
             query = query.order_by(Post.created_at.asc())
